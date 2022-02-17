7600a8a232234fe8f70ce5e9251d4868aa581234
PEDRO DE OLIVEIRA ROCHA <pedro.orocha@senacsp.edu.br>
2022-02-16 22:10:45 -0300

    Final da aula

---
+++ script.js
@@ -0,0 +1,38 @@
+//Ola Boninho :D
+
+/* 
+    Ola Boninho :D
+    Ola Boninho :D
+    Ola Boninho :D
+*/
+
+function diretoNoElemento() {
+    console.log('Olá mundo!');
+}
+
+let x = 20;
+const XPTO = 20;
+
+function profBoninho() {
+
+    if (x == 20)
+        console.log("bbb");
+    else
+        console.log("aaa");
+
+    for (i = 0; i < 10; i++) {
+        console.log('Boninho');
+    }
+}
+
+profBoninho();
+
+
+
+x = x + ' Programar!';
+
+
+
+
+console.log(XPTO);
+console.log(x);
