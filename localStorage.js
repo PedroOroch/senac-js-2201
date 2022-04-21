cf7bc15487913532a5398e3f2a1b991fc67cb3f2
Oroch Pedro <oliveirarocha_pedro@hotmail.com>
2022-04-20 22:08:52 -0300

    Lista de tarefas Pre-finished

--- localStorage.js
+++ localStorage.js
@@ -1,12 +1,12 @@
 //De objeto JSON para string
 const OBJ = { nome: "Bono", idade: 37 }
 
-console.log(JSON.stringify(OBJ));
+//console.log(JSON.stringify(OBJ));
 
 //De string ara objeto JSON
 const STR = '{"nome": "Bono", "idade": 37}';
 
-console.log(JSON.parse(STR));
+//console.log(JSON.parse(STR));
 
 //OBJ.fazCompra();
 
@@ -15,14 +15,16 @@ localStorage.setItem('nome', 'Bono');
 
 const NOME = localStorage.getItem('nome');
 
-console.log(NOME);
+//console.log(NOME);
 
 sessionStorage.setItem('idade', '37');
 
 const IDADE = sessionStorage.getItem('idade');
 
-console.log(IDADE);
+//console.log(IDADE);
 
+
+/*
 document.querySelector('input.btn').addEventListener('click', function(evento) {
     //alert('Voce clicou em mim ( ͡° ͜ʖ ͡°)');
     evento.preventDefault(); //previne que algo não desejável aconteça
@@ -36,8 +38,34 @@ document.querySelector('input.btn').addEventListener('click', function(evento) {
 
 
 });
+*/
 
 const TRF = localStorage.getItem('tarefa');
 
 if (TRF)
     document.querySelector('#exibe').innerText = TRF;
+
+
+//Recupera o valor do input preenchido pelo usuário e adiciona no storage
+document.querySelector('input.btn').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const TAREFA = document.querySelector('#task').value;
+
+    let tarefas;
+
+    if (localStorage.getItem('tarefas') === null)
+        tarefas = [];
+    else
+        tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    tarefas.push(TAREFA);
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+    console.log(tarefas);
+
+    document.getElementById('task').value = '';
+});
+
+//Para sessionStorage é sótrocar todos localStorage para sessionStorage
